ec148c13de0e4a837420eef6b8de3f04d93983be
Kowshar Ahmed <kowshar@joomshaper.com>
2015-02-07 02:41:13 +0600

    Fixed vm issue and update copyright

--- elements/vmcategories.php
+++ elements/vmcategories.php
@@ -1,73 +1,54 @@
 <?php
 defined('_JEXEC') or die();
 
-/*
-# News Show SP2 - News display/Slider module by JoomShaper.com
-# Author    JoomShaper http://www.joomshaper.com
-# Copyright (C) 2010 - 2014 JoomShaper.com. All Rights Reserved.
-# @license - GNU/GPL V2 or later
-# Websites: http://www.joomshaper.com
-*/
-
-if (JFile::exists(JPATH_ROOT . '/administrator/components/com_virtuemart/helpers/config.php')) {
-if (!class_exists('VmConfig'))
-    require(JPATH_ROOT . '/administrator/components/com_virtuemart/helpers/config.php');
+/**
+ *
+ * @package    VirtueMart
+ * @subpackage Plugins  - Elements
+ * @author Valérie Isaksen
+ * @link http://www.virtuemart.net
+ * @copyright Copyright (c) 2004 - 2011 VirtueMart Team. All rights reserved.
+ * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
+ * VirtueMart is free software. This version may have been modified pursuant
+ * to the GNU General Public License, and as distributed it includes or
+ * is derivative of works licensed under the GNU General Public License or
+ * other free or open source software licenses.
+ * @version $Id$
+ */
+defined('DS') or define('DS', DIRECTORY_SEPARATOR);
+if (!class_exists( 'VmConfig' )) require(JPATH_ROOT.DS.'administrator'.DS.'components'.DS.'com_virtuemart'.DS.'helpers'.DS.'config.php');
 
 if (!class_exists('ShopFunctions'))
-    require(JPATH_VM_ADMINISTRATOR . '/helpers/shopfunctions.php');
+	require(VMPATH_ADMIN . DS . 'helpers' . DS . 'shopfunctions.php');
 if (!class_exists('TableCategories'))
-    require(JPATH_VM_ADMINISTRATOR . '/tables/categories.php');
+	require(VMPATH_ADMIN . DS . 'tables' . DS . 'categories.php');
+jimport('joomla.form.formfield');
 
-if (!class_exists('VmElements'))
-    require(JPATH_VM_ADMINISTRATOR . '/elements/vmelements.php');
 /*
  * This element is used by the menu manager
  * Should be that way
  */
-class VmElementVmCategories extends VmElements {
+class JFormFieldVmcategories extends JFormField {
 
-    var $type = 'vmcategories';
+	var $type = 'vmcategories';
 
-    // This line is required to keep Joomla! 1.6/1.7 from complaining
-    function getInput() {
-        $key = ($this->element['key_field'] ? $this->element['key_field'] : 'value');
-        $val = ($this->element['value_field'] ? $this->element['value_field'] : $this->name);
-        JPlugin::loadLanguage('com_virtuemart', JPATH_ADMINISTRATOR);
-        $categorylist = ShopFunctions::categoryListTree(array($this->value));
-        $class = '';
-        $html = '<select class="inputbox" name="' . $this->name . '" >';
-        $html .= '<option value="0">' . JText::_('COM_VIRTUEMART_CATEGORY_FORM_TOP_LEVEL') . '</option>';
-        $html .= $categorylist;
-        $html .="</select>";
-        return $html;
-    }
+	// This line is required to keep Joomla! 1.6/1.7 from complaining
+	protected function getInput() {
+		$key = ($this->element['key_field'] ? $this->element['key_field'] : 'value');
+		$val = ($this->element['value_field'] ? $this->element['value_field'] : $this->name);
 
-    function fetchElement($name, $value, &$node, $control_name) {
-        JPlugin::loadLanguage('com_virtuemart', JPATH_ADMINISTRATOR);
-        $categorylist = ShopFunctions::categoryListTree(array($value));
+		VmConfig::loadJLang('com_virtuemart');
 
-        $class = '';
-        $html = '<select class="inputbox" name="' . $control_name . '[' . $name . ']' . '" >';
-        $html .= '<option value="0">' . JText::_('COM_VIRTUEMART_CATEGORY_FORM_TOP_LEVEL') . '</option>';
-        $html .= $categorylist;
-        $html .="</select>";
-        return $html;
-    }
+		$categorylist = ShopFunctions::categoryListTree(array($this->value));
+
+		$html = '<select class="inputbox"   name="' . $this->name . '" >';
+		$html .= '<option value="0">' . vmText::_('COM_VIRTUEMART_CATEGORY_FORM_TOP_LEVEL') . '</option>';
+		$html .= $categorylist;
+		$html .= "</select>";
+		return $html;
+	}
+
 
 }
 
 
-if (JVM_VERSION === 2 ) {
-
-    class JFormFieldVmCategories extends VmElementVmCategories {
-
-    }
-
-} else {
-
-    class JElementVmCategories extends VmElementVmCategories {
-
-    }
-
-}
-}
